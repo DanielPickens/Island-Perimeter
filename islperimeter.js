285bd50c47427b56fb2293dfe69f29b06526f58e
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-10-15 22:51:54 -0600

    Create islperimeter.js

---
+++ islperimeter.js
@@ -0,0 +1,32 @@
+const islandPerimeter = function(grid) {
+    let p = 0;
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 0) continue;
+                
+                if(i - 1 < 0 || grid[i - 1][j] == 0) {
+                    p++;
+                }
+                
+                if(j - 1 < 0 || grid[i][j - 1] == 0) {
+                    p++;
+                }
+
+                if(i + 1 > grid.length - 1 || grid[i + 1][j] == 0) {
+                    p++;
+                }
+
+                if(j + 1 > grid[i].length - 1 || grid[i][j + 1] == 0) {
+                    p++;
+                }
+            }
+            
+        }
+        return p;
+    }
+
+
+/*
+Runtime: 164 ms, faster than 89.04% of JavaScript online submissions for Island Perimeter.
+Memory Usage: 48 MB, less than 75.24% of JavaScript online submissions for Island Perimeter.
+*/
